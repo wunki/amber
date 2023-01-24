51df18f59cb3aef6c5f098b62de89fc87703a2c8
Petar Radosevic <petar@petar.dev>
2023-01-24 06:46:57 +0100

    define the token type

---
+++ src/token.zig
@@ -0,0 +1,24 @@
+const Token = union(enum) {
+    illegal,
+    eof,
+
+    identifier: []const u8,
+    intLiteral: usize,
+
+    // Operators
+    assign,
+    plus,
+
+    // Delimiters
+    comma,
+    semicolon,
+
+    lparen,
+    rparen,
+    lbrace,
+    rbrace,
+
+    // Keywords
+    function,
+    let,
+};
